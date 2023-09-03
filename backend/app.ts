b631815aa1bf84654c8b4312ba86958ff20db5e8
Ron Winchester <rom0203@gmail.com>
2023-09-03 22:53:07 +0300

    backend: взаимодействие ws

--- backend/app.ts
+++ backend/app.ts
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import dotenv from "dotenv";
 import cors from "cors";
 import router from "./route";
+import User from "./user";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(cors({ origin: process.env.DOMAIN }));
 app.use(router);
 
 const server = http.createServer(app);
+
 const io = new Server(server, {
 	cors: {
 		origin: process.env.DOMAIN,
@@ -21,6 +23,63 @@ const io = new Server(server, {
 	},
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+	const userInstance = new User();
+
+	socket.on("join", (res) => {
+		socket.join(res.room);
+
+		const { user, isExist } = userInstance.addUser({
+			name: res.name.trim().toLowerCase(),
+			room: res.room.trim().toLowerCase(),
+		});
+
+		const userMessage = isExist
+			? `${user.name}, с возвращением`
+			: `Привет, ${user.name}`;
+
+		socket.emit("message", {
+			data: { user: { name: "Admin" }, message: userMessage },
+		});
+
+		socket.broadcast.to(user.room).emit("message", {
+			data: { user: { name: "Admin" }, message: `${user.name} присоединился` },
+		});
+
+		io.to(user.room).emit("room", {
+			data: { users: userInstance.getRoomUsers(user.room) },
+		});
+	});
+
+	socket.on("sendMessage", ({ message, params }) => {
+		const user = userInstance.findUser(params);
+
+		if (user) {
+			io.to(user.room).emit("message", { data: { user, message } });
+		}
+	});
+
+	socket.on("leftRoom", ({ params }) => {
+		const user = userInstance.removeUser(params);
+
+		if (user) {
+			const { room, name } = user;
+
+			io.to(room).emit("message", {
+				data: { user: { name: "Admin" }, message: `${name} покинул комнату` },
+			});
+
+			io.to(room).emit("room", {
+				data: { users: userInstance.getRoomUsers(room) },
+			});
+		}
+	});
+
+	io.on("disconnect", () => {
+		console.log("Disconnect");
+	});
+});
+
+server.listen(PORT, () => {
 	console.log("Сервер запущен!");
 });
---
+++ backend/user.ts
@@ -0,0 +1,45 @@
+type UserType = {
+	name: string;
+	room: string;
+};
+
+class User {
+	users: Array<UserType>;
+
+	constructor() {
+		this.users = [];
+	}
+
+	public addUser = (user: UserType) => {
+		const isExist = this.findUser(user);
+
+		if (!isExist) {
+			this.users.push(user);
+		}
+
+		const currentUser = isExist || user;
+
+		return { isExist: !!isExist, user: currentUser };
+	};
+
+	public findUser = (user: UserType): UserType | undefined => {
+		return this.users.find((u) => u.name === user.name && u.room === user.room);
+	};
+
+	public getRoomUsers = (room: string) =>
+		this.users.filter((u) => u.room === room);
+
+	public removeUser = (user: UserType) => {
+		const found = this.findUser(user);
+
+		if (found) {
+			this.users = this.users.filter(
+				({ room, name }) => room === found.room && name !== found.name
+			);
+		}
+
+		return found;
+	};
+}
+
+export default User;
